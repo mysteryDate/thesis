3b75b2acca01f3c3c852d626a023bb4acd70e99e
Aaron Krajeski <aaronkrajeski@gmail.com>
2013-06-21 14:09:24 -0400

    Finished introduction subsection, added user feedback file. Added header files for code and compiling outside of main thesis.tex file.

--- paper/cleanup.sh
+++ paper/cleanup.sh
@@ -6,4 +6,16 @@ rm *.log
 rm *.lot
 rm *.toc
 rm *.blg
+rm *.fdb_latexmk
+rm *.bbl
+rm *.fls
+rm *.synctex.gz
+
+rm chapters/*.aux
+rm chapters/*.log
+rm chapters/*.fdb_latexmk
+rm chapters/*.bbl
+rm chapters/*.fls
+rm chapters/*.synctex.gz
+
 
